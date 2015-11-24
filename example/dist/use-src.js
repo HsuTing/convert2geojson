7f8d12ed2100df8ba08234f07faffc81db386a24
hsuting <hsuting0106@gmail.com>
2015-11-24 14:52:54 +0800

    add use src

---
+++ example/dist/use-src.js
@@ -0,0 +1,23 @@
+var config = {
+  output: {
+    filename: '[name].geojson',
+    path: './data/'
+  },
+  simple: {
+    id: "map",
+    center: {
+      lat: 23.619, 
+      lon: 120.795,
+      zoom: {
+        normal: 10,
+        min: 1,
+        max: 17
+      }
+    },
+    include: [
+      {'data': {}}
+    ]
+  }
+}
+
+convert2geojson.Map(config);
