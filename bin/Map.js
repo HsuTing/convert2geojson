6a12b50ee287069d6452a7336f77459da594eb08
hsuting <hsuting0106@gmail.com>
2015-11-25 16:43:06 +0800

    fixed bug: 'Map.js' need 'style.load'

--- bin/Map.js
+++ bin/Map.js
@@ -1,9 +1,8 @@
 'use strict';
 
-var Map = require('./../convert2geojson.js').Map;
+var Map = require('convert2geojson').Map;
 var Config = require('./../../../convert2geojson.config.js');
 
 (function() {
-  $('body').html('<div id ="' + Config.simple.id + '"></div>');
   Map(Config); 
 })();
